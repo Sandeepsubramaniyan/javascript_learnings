18f68d5414842e3f50e888cdc57b053c10e633d9
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-03-04 18:48:40 +0530

    Benefit of Callback Function

---
+++ benefits_of_calling_func.js
@@ -0,0 +1,17 @@
+// The benefit of using a callback function is that you can 
+// wait for the result of a previous function call 
+// and then execute another function call.
+
+//Program with setTimeout()
+
+function greet() {
+    console.log("hello world")
+}
+
+function sayName(name) {
+    console.log("i am "+ name)
+}
+
+//calling the functions
+setTimeout(greet,1000)
+sayName("sandeep")
